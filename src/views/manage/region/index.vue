b49b6f1fae91353089bbd534eeda23ff82b78a07
MeiDaiAa <114286638+MeiDaiAa@users.noreply.github.com>
2025-09-11 10:25:48 +0800

    feat(emp): 生成人员列表代码

--- src/views/manage/region/index.vue
+++ src/views/manage/region/index.vue
@@ -26,7 +26,7 @@
         <el-button type="warning" plain icon="Download" @click="handleExport"
           v-hasPermi="['manage:region:export']">导出</el-button>
       </el-col>
-      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
+      <right-toolbar v-model:showSearch="showSearch" @queryTable="getLgist"></right-toolbar>
     </el-row>
 
     <el-table v-loading="loading" :data="regionList" @selection-change="handleSelectionChange">
